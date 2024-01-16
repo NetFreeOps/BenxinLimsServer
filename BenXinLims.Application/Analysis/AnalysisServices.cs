52b1fd7fccc4308d44a35c497da6b8d8f6d69cae
benxin <377079544@qq.com>
2024-01-16 21:30:26 +0800

    添加分析类函数占位

---
+++ BenXinLims.Application/Analysis/AnalysisServices.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenXinLims.Application.Analysis
+{
+    public class AnalysisServices:IDynamicApiController,ITransient
+    {
+        // 建立新分析
+        public Task<string> createAnalysis(string analysisName)
+        {
+            return Task.FromResult("OK");
+        }
+        // 删除分析
+        public Task<string> deleteAnalysis(string analysisName)
+        {
+            return Task.FromResult("OK");
+        }
+        // 获取分析列表
+        public Task<string> getAnalysisList()
+        {
+            return Task.FromResult("OK");
+        }
+        // 更新分析信息
+        public Task<string> updateAnalysis(string analysisName)
+        {
+            return Task.FromResult("OK");
+        }
+        // 为分析添加分项
+        public Task<string> addItemToAnalysis(string analysisName)
+        {
+            return Task.FromResult("OK");
+        }
+        // 为分析删除分项
+        public Task<string> deleteItemFromAnalysis(string analysisName)
+        {
+            return Task.FromResult("OK");
+        }
+        // 获取分析的分项列表
+        public Task<string> getItemListFromAnalysis(string analysisName)
+        {
+            return Task.FromResult("OK");
+        }
+        // 更新分析的分项信息
+        public Task<string> updateItemFromAnalysis(string analysisName)
+        {
+            return Task.FromResult("OK");
+        }
+        // 为分析分配人员
+        public Task<string> addUserToAnalysis(string analysis)
+        {
+            return Task.FromResult("OK");
+        }
+        // 为分析删除人员
+        public Task<string> deleteUserFromAnalysis(string analysis)
+        {
+            return Task.FromResult("OK");
+        }
+        // 获取分析的人员列表
+        public Task<string> getUserListFromAnalysis(string analysis)
+        {
+            return Task.FromResult("OK");
+        }
+        // 为分析添加设备
+        public Task<string> addInsToAnalysis(string analysis)
+        {
+            return Task.FromResult("OK");
+        }
+        // 为分析删除设备
+        public Task<string> deleteInsFromAnalysis(string analysis)
+        {
+            return Task.FromResult("OK");
+        }
+
+    }
+}
