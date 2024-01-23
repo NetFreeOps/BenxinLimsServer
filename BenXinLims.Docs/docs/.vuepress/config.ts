206765143a4b6cfda64d576c63d7470567d237c1
本心 <377079544@qq.com>
2024-01-23 16:35:15 +0800

    更新配置文件

--- BenXinLims.Docs/docs/.vuepress/config.ts
+++ BenXinLims.Docs/docs/.vuepress/config.ts
@@ -1,6 +1,5 @@
 import { defaultTheme, defineUserConfig } from 'vuepress'
 import { viteBundler } from '@vuepress/bundler-vite'
-import { webpackBundler } from '@vuepress/bundler-webpack'
 
 
 
@@ -9,10 +8,10 @@ export default defineUserConfig({
   title: '本心LIMS',
   description: '一个小型LIMS',
   base:'/BenxinLimsServer/tree/main/BenXinLims.Docs/',
-  // specify bundler via environment variable
-  bundler:
-    process.env.DOCS_BUNDLER === 'webpack' ? webpackBundler() : viteBundler(),
-    
+  bundler: viteBundler({
+    viteOptions: {},
+    vuePluginOptions: {},
+  }),
   theme: defaultTheme({
     sidebarDepth: 3,
     logo: '/logo.png',
