7f01e4fd855eeed3aff06915325b1cc187beedf6
本心 <377079544@qq.com>
2024-01-23 10:11:50 +0800

    文档配置完成

---
+++ BenXinLims.Docs/docs/.vuepress/config.ts
@@ -0,0 +1,38 @@
+import { defaultTheme, defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: '本心LIMS',
+  description: '一个小型LIMS',
+
+  theme: defaultTheme({
+    sidebarDepth: 3,
+    logo: '/logo.png',
+    home: '/',
+    repo: 'https://github.com/NetFreeOps/BenxinLimsServer',
+    navbar: [{ text: "首页", link: "/" }, { text: "文档", link: "/function-system/sampleFunction.md" }],
+    sidebar: [
+      {
+        text: "系统引导",
+        link: '/guide/',
+        children: [
+          { text: "系统介绍", link: "/guide/introduction.md" },
+          { text: "快速开始", link: "/guide/quickStart.md" },
+          { text: "数据库配置", link: "/guide/dbConfig.md" },
+          { text: "缓存配置", link: "/guide/cacheConfig.md" },]
+      },
+      {
+        text: "内置函数",
+        link: '/function-system/',
+        children: [
+          { text: "样品函数", link: "/function-system/sampleFunction.md" },
+          { text: "检测函数", link: "/function-system/testFunction.md" },
+          { text: "结果函数", link: "/function-system/resultFunction.md" },
+          { text: "分析方法函数", link: "/function-system/analysisFunction.md" },
+          { text: "设备函数", link: "/function-system/instrumentFunction.md" },
+          { text: "人员函数", link: "/function-system/userFunction.md" }]
+
+      }
+    ]
+  })
+})
---
+++ BenXinLims.Docs/docs/README.md
@@ -0,0 +1,25 @@
+---
+home: true
+modules:
+  - BannerBrand
+  - MdContent
+  - Footer
+bannerBrand:
+  # bgImage: '/bg.svg'
+  title: 本心
+  description: 小微型LIMS。
+  tagline: 
+  buttons:
+    - { text: github, icon: 'LogoGithub', link: 'https://github.com/NetFreeOps/BenxinLimsServer' }
+    - { text: Default Style, link: '/docs/style-default-api/introduce', type: 'plain' }
+  # socialLinks:
+  #   - { icon: 'LogoGithub', link: 'https://github.com/vuepress-reco/vuepress-theme-reco' }
+# blog:
+#   socialLinks:
+#     - { icon: 'LogoGithub', link: 'https://github.com/recoluan' }
+isShowTitleInHome: true
+actionText: About
+actionLink: /views/other/about
+---
+
+
