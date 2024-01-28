5175270bef45a6bc1926bc753a19a367fcf8b5d4
本心 <377079544@qq.com>
2024-01-28 16:53:42 +0800

    添加几个登录占位符

---
+++ BenXinLims.Core/Services/sysAuthService.cs
@@ -0,0 +1,52 @@
+﻿using Furion.DynamicApiController;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenXinLims.Core.Services
+{
+    /// <summary>
+    /// 认证服务
+    /// </summary>
+    public class sysAuthService:IDynamicApiController
+    {
+        // web登录
+        public Task<string> webLogin(string username,string password)
+        {
+            return Task.FromResult("webLogin");
+        }
+        // app登录
+        public Task<string> appLogin(string username, string password)
+        {
+            return Task.FromResult("appLogin");
+        }
+        // 微信登录
+        public Task<string> wechatLogin(string username, string password)
+        {
+            return Task.FromResult("wechatLogin");
+        }
+        // 钉钉登录
+        public Task<string> dingdingLogin(string username, string password)
+        {
+            return Task.FromResult("dingdingLogin");
+        }
+        // 第三方API对接登录
+        public Task<string> apiLogin(string username, string password)
+        {
+            return Task.FromResult("apiLogin");
+        }
+        // web登出
+        public Task<string> webLogout(string username)
+        {
+            return Task.FromResult("webLogout");
+        }
+        // app登出
+        public Task<string> appLogout(string username)
+        {
+            return Task.FromResult("appLogout");
+        }
+
+    }
+}
