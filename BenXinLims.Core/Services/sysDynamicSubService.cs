539abd3044a1d432383e222513cdd81bebc1f2bc
本心 <377079544@qq.com>
2024-02-19 14:39:05 +0800

    待解决：动态添加API报错，swagger无法正常刷新

---
+++ BenXinLims.Core/Services/sysDynamicSubService.cs
@@ -0,0 +1,63 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenXinLims.Core.Services
+{
+    /// <summary>
+    /// 动态接口服务
+    /// </summary>
+    public class sysDynamicSubService:IDynamicApiController
+    {
+        private readonly IDynamicApiRuntimeChangeProvider _changeProvider;
+
+        public sysDynamicSubService(IDynamicApiRuntimeChangeProvider changeProvider)
+        {
+            _changeProvider = changeProvider;
+        }
+
+        /// <summary>
+        /// 动态添加 WebAPI/Controller
+        /// </summary>
+        /// <param name="csharpCode"></param>
+        /// <param name="assemblyName">可自行指定程序集名称</param>
+        /// <returns></returns>
+       
+        public string Compile([FromBody] string csharpCode, [FromQuery] string assemblyName = default)
+        {
+            
+            // 拼接代码
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("using Furion.DynamicApiController;");
+            sb.AppendLine("namespace BenXinLims.Core.Services");
+            sb.Append("{");
+            sb.AppendLine("public class userSub : IDynamicApiController");
+            sb.AppendLine("{");
+            sb.AppendLine(csharpCode);
+            sb.AppendLine("}}");
+
+
+            // 编译 C# 代码并返回动态程序集
+            var dynamicAssembly = App.CompileCSharpClassCode(sb.ToString(), assemblyName);
+
+            // 将程序集添加进动态 WebAPI 应用部件
+            _changeProvider.AddAssembliesWithNotifyChanges(dynamicAssembly);
+
+            // 返回动态程序集名称
+            return dynamicAssembly.GetName().Name;
+        }
+
+        /// <summary>
+        /// 移除动态程序集 WebAPI/Controller
+        /// </summary>
+        public void Remove(string assemblyName)
+        {
+            _changeProvider.RemoveAssembliesWithNotifyChanges(assemblyName);
+        }
+
+    }
+}
