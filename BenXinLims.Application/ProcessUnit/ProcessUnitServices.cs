c9fb2097b4c8cfa65257edea4a4fd69fd657d399
本心 <377079544@qq.com>
2024-07-09 16:42:44 +0800

    开始添加装置管理服务

---
+++ BenXinLims.Application/ProcessUnit/ProcessUnitServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenXinLims.Application.ProcessUnit
+{
+    /// <summary>
+    /// 装置层级管理-从最上级开始，逐级到最下级
+    /// </summary>
+    public class ProcessUnitServices
+    {
+
+    }
+}
