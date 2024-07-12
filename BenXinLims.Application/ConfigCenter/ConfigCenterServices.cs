6915f9a269e87024f4500d89c69179ff9e0f1d43
benxin <377079544@qq.com>
2024-07-12 21:30:27 +0800

    feat(application): 添加用户岗位对应关系服务及实体类
    - 新增PostServices，提供用户岗位对应关系的添加、查询和删除等功能。
    - 引入UserPostEntry实体类，用于表示用户和岗位的对应关系。
    - 添加userQueryDto和postQueryDto，用于参数化查询用户和岗位。

---
+++ BenXinLims.Application/ConfigCenter/ConfigCenterServices.cs
@@ -0,0 +1,67 @@
+﻿using BenXinLims.Core;
+using BenXinLims.Core.Entry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenXinLims.Application.ConfigCenter
+{
+    /// <summary>
+    /// 配置中心
+    /// </summary>
+    public class ConfigCenterServices:IDynamicApiController
+    {
+        /// <summary>
+        /// 查找配置
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        public async Task<List<sysUserConfigEntry>> GetConfig([FromQuery] UserConfigQueryModel query)
+        {
+            var db = DbContext.Instance;
+            var list = await db.Queryable<sysUserConfigEntry>()
+                .WhereIF(!string.IsNullOrEmpty(query.UserId), a => a.UserId == query.UserId)
+                .WhereIF(!string.IsNullOrEmpty(query.ConfigType), a => a.ConfigType == query.ConfigType)
+                .WhereIF(!string.IsNullOrEmpty(query.ConfigField), a => a.ConfigField == query.ConfigField)
+                .ToListAsync();
+            return list;
+        }
+        /// <summary>
+        /// 批量更新或插入配置项
+        /// </summary>
+        /// <param name="configs"></param>
+        /// <returns></returns>
+        public async Task<int> UpdateConfig( List<sysUserConfigEntry> configs)
+        {
+            var db = DbContext.Instance;
+            foreach (var config in configs)
+            {
+                var oldConfig = await db.Queryable<sysUserConfigEntry>().Where(a => a.UserId == config.UserId && a.ConfigType == config.ConfigType && a.ConfigField == config.ConfigField).FirstAsync();
+                if (oldConfig == null)
+                {
+                    await db.Insertable(configs).ExecuteCommandAsync();
+                }
+                else
+                {
+                    await db.Updateable(config).ExecuteCommandAsync();
+                }
+            }
+            return configs.Count;
+        }
+
+        /// <summary>
+        /// 删除配置项
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<int> DeleteConfig(int id)
+        {
+            var db = DbContext.Instance;
+          
+           return await db.Deleteable<sysUserConfigEntry>().Where(it =>it.Id == id).ExecuteCommandAsync();
+           
+        }
+    }
+}
---
+++ BenXinLims.Application/ConfigCenter/UserConfigEntry.cs
@@ -0,0 +1,57 @@
+﻿using BenXinLims.Core.Entry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenXinLims.Application.ConfigCenter
+{
+    /// <summary>
+    /// 用户配置中心
+    ///</summary>
+    [SugarTable("sys_user_config")]
+    public class SysUserConfig:dbEntryBase
+    {
+        /// <summary>
+        /// 主键 
+        ///</summary>
+        [SugarColumn(ColumnName = "id", IsPrimaryKey = true, IsIdentity = true)]
+        public int Id { get; set; }
+        /// <summary>
+        /// 用户工号，关联sys_user 
+        ///</summary>
+        [SugarColumn(ColumnName = "user_id")]
+        public string UserId { get; set; }
+        /// <summary>
+        /// 配置类型 
+        ///</summary>
+        [SugarColumn(ColumnName = "config_type")]
+        public string ConfigType { get; set; }
+        /// <summary>
+        /// 配置字段名 
+        ///</summary>
+        [SugarColumn(ColumnName = "config_field")]
+        public string ConfigField { get; set; }
+        /// <summary>
+        /// 配置字段值 
+        ///</summary>
+        [SugarColumn(ColumnName = "config_value")]
+        public string ConfigValue { get; set; }
+        /// <summary>
+        /// 状态 
+        ///</summary>
+        [SugarColumn(ColumnName = "status")]
+        public string Status { get; set; }
+      
+    }
+    /// <summary>
+    /// 配置查询实体类
+    /// </summary>
+    public class UserConfigQueryModel
+    {
+        public string? UserId { get; set; }
+        public string? ConfigType { get; set; }
+        public string? ConfigField { get; set; }
+    }
+}
