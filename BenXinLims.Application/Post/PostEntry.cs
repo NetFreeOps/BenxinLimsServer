c107104acbfe31cf93511f29ad3ffb6a31398df7
benxin <377079544@qq.com>
2024-07-20 21:37:55 +0800

    feat(BenXinLims.Application): 岗位管理功能开发
    新增了岗位表的管理功能，包括岗位信息的增删改查操作。
    同时，对PostServices类进行了相关方法的实现，以支持岗位管理的业务逻辑。

---
+++ BenXinLims.Application/Post/PostEntry.cs
@@ -0,0 +1,44 @@
+﻿using BenXinLims.Core.Entry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenXinLims.Application.Post
+{
+    /// <summary>
+    /// 岗位表
+    ///</summary>
+    [SugarTable("post")]
+    public class PostEntry:dbEntryBase
+    {
+        /// <summary>
+        ///  
+        ///</summary>
+        [SugarColumn(ColumnName = "id", IsPrimaryKey = true)]
+        public int Id { get; set; }
+        /// <summary>
+        ///  
+        ///</summary>
+        [SugarColumn(ColumnName = "post_name")]
+        public string PostName { get; set; }
+        /// <summary>
+        ///  
+        ///</summary>
+        [SugarColumn(ColumnName = "post_code")]
+        public string PostCode { get; set; }
+        /// <summary>
+        ///  
+        ///</summary>
+        [SugarColumn(ColumnName = "description")]
+        public string Description { get; set; }
+        /// <summary>
+        ///  
+        ///</summary>
+        [SugarColumn(ColumnName = "active")]
+        public int Active { get; set; }
+       
+
+    }
+}
--- BenXinLims.Application/Post/PostServices.cs
+++ BenXinLims.Application/Post/PostServices.cs
@@ -1,4 +1,5 @@
 ﻿using BenXinLims.Core;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,79 @@ namespace BenXinLims.Application.Post
     /// </summary>
     public class PostServices:IDynamicApiController
     {
+        /// <summary>
+        /// 获取所有岗位
+        /// </summary>
+        /// <param name="post"></param>
+        /// <returns></returns>
+        public async Task<dynamic> GetPostsList(PostEntry post)
+        {
+            var db = DbContext.Instance;
+            var list = await db.Queryable<PostEntry>().ToListAsync();
+            return list;
+        }
+        /// <summary>
+        /// 更新岗位信息
+        /// </summary>
+        /// <param name="post"></param>
+        /// <returns></returns>
+        public async Task<int> UpdatePost(PostEntry post)
+        {
+            // 岗位代码不能重复
+            var db = DbContext.Instance;
+            if(await db.Queryable<PostEntry>().Where(it => it.PostCode == post.PostCode && it.Id != post.Id).AnyAsync())
+            {
+                throw Oops.Oh("岗位代码重复");
+            }
+            // 已经分配岗位的不能更改
+            if(await db.Queryable<UserPostEntry>().Where(it => it.PostName == post.PostName ).AnyAsync())
+            {
+                throw Oops.Oh("该岗位已经使用，不允许更改");
+            }
+            // 岗位名称不能重复
+            if (await db.Queryable<PostEntry>().Where(it => it.PostName == post.PostName && it.Id != post.Id).AnyAsync())
+            {
+                throw Oops.Oh("岗位名称重复");
+            }
+            return await db.Updateable(post).ExecuteCommandAsync();
+        }
+       /// <summary>
+       /// 添加岗位
+       /// </summary>
+       /// <param name="post"></param>
+       /// <returns></returns>
+        public async Task<int> AddPost(PostEntry post)
+        {
+            var db = DbContext.Instance;
+            // 检查岗位名称、代码是否存在
+            if (await db.Queryable<PostEntry>().Where(a => a.PostName == post.PostName).AnyAsync())
+            {
+                throw Oops.Oh("岗位名称重复");
+            }
+            if (await db.Queryable<PostEntry>().Where(it => it.PostCode == post.PostCode).AnyAsync())
+            {
+                throw Oops.Oh("岗位代码重复");
+            }
+            return await db.Insertable(post).ExecuteReturnIdentityAsync();
+        }
+
+        /// <summary>
+        /// 删除岗位
+        /// </summary>
+        /// <param name="post"></param>
+        /// <returns></returns>
+        public async Task<int> DeletePost(PostEntry post)
+        {
+            var db = DbContext.Instance;
+            //已经分配岗位不允许删除
+            if (await db.Queryable<UserPostEntry>().Where(it => it.PostName == post.PostName).AnyAsync())
+            {
+                throw Oops.Oh("该岗位已经使用，不允许删除");
+            }
+            //删除岗位
+            return await db.Deleteable<PostEntry>().Where(it => it.PostName == post.PostName).ExecuteCommandAsync();
+        }
+
         /// <summary>
         /// 根据用户查询用户岗位
         /// </summary>
