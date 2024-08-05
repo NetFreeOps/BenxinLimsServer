87335b2eb8a81fda0c3ca16d7ac3884423811b1d
benxin <377079544@qq.com>
2024-08-05 21:47:04 +0800

    "添加模板功能的注释说明"
    此次更改详细添加了关于模板功能各个部分的注释说明，包括模板实体类、模板分项实体类及其属性的描述。此外，还包含了模板服务类及其方法的注释，以便于理解和使用。

---
+++ BenXinLims.Application/Template/templateEntry.cs
@@ -0,0 +1,111 @@
+﻿using BenXinLims.Core.Entry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenXinLims.Application.Template
+{
+    /// <summary>
+    /// 模板实体类
+    ///</summary>
+    [SugarTable("template")]
+    public class TemplateEntry:dbEntryBase
+    {
+        /// <summary>
+        /// 主键 
+        ///</summary>
+        [SugarColumn(ColumnName = "id", IsPrimaryKey = true, IsIdentity = true)]
+        public int Id { get; set; }
+        /// <summary>
+        /// 模板名称，唯一 
+        ///</summary>
+        [SugarColumn(ColumnName = "template_name")]
+        public string TemplateName { get; set; }
+        /// <summary>
+        /// 模板类型 
+        ///</summary>
+        [SugarColumn(ColumnName = "template_type")]
+        public string TemplateType { get; set; }
+        /// <summary>
+        /// 描述信息 
+        ///</summary>
+        [SugarColumn(ColumnName = "description")]
+        public string Description { get; set; }
+        
+    }
+
+    /// <summary>
+    /// 模板分项实体类
+    ///</summary>
+    [SugarTable("template_item")]
+    public class TemplateItemEntry:dbEntryBase
+    {
+        /// <summary>
+        /// 主键 
+        ///</summary>
+        [SugarColumn(ColumnName = "id", IsPrimaryKey = true, IsIdentity = true)]
+        public int Id { get; set; }
+        /// <summary>
+        /// 模板ID 
+        ///</summary>
+        [SugarColumn(ColumnName = "template_id")]
+        public string TemplateId { get; set; }
+        /// <summary>
+        /// 模板名称 
+        ///</summary>
+        [SugarColumn(ColumnName = "template_name")]
+        public string TemplateName { get; set; }
+        /// <summary>
+        /// 配置字段名 
+        ///</summary>
+        [SugarColumn(ColumnName = "template_field")]
+        public string TemplateField { get; set; }
+        /// <summary>
+        /// 字段类型 
+        ///</summary>
+        [SugarColumn(ColumnName = "fiele_type")]
+        public string FieleType { get; set; }
+        /// <summary>
+        /// 默认值 
+        ///</summary>
+        [SugarColumn(ColumnName = "default_value")]
+        public string DefaultValue { get; set; }
+        /// <summary>
+        /// 公式型代码 
+        ///</summary>
+        [SugarColumn(ColumnName = "source_code")]
+        public string SourceCode { get; set; }
+        /// <summary>
+        /// 列表型的列表名称 
+        ///</summary>
+        [SugarColumn(ColumnName = "list_name")]
+        public string ListName { get; set; }
+        /// <summary>
+        /// 列表型取值字段 
+        ///</summary>
+        [SugarColumn(ColumnName = "list_field")]
+        public string ListField { get; set; }
+        /// <summary>
+        /// 状态 
+        ///</summary>
+        [SugarColumn(ColumnName = "status")]
+        public string Status { get; set; }
+       
+    }
+    /// <summary>
+    /// 数据库表结构
+    /// </summary>
+    public class dbField
+    {
+        public string FieldName { get; set; }
+        public string FieldType { get; set; }
+        public string FieldLength { get; set; }
+        public string FieldDecimal { get; set; }
+        public string FieldDefault { get; set; }
+        public string FieldNull { get; set; }
+        public string FieldComment { get; set; }
+    }
+   
+}
---
+++ BenXinLims.Application/Template/templateServices.cs
@@ -0,0 +1,137 @@
+﻿using BenXinLims.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BenXinLims.Application.Template
+{
+    /// <summary>
+    /// 模板服务
+    /// </summary>
+    public class templateServices : IDynamicApiController, ITransient
+    {
+        // 注入数据库上下文
+        private readonly ISqlSugarClient _dbContext;
+        public templateServices(ISqlSugarClient dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        /// <summary>
+        /// 获取数据库表结构
+        /// </summary>
+        /// <param name="tableName"></param>
+        /// <returns></returns>
+        public List<DbColumnInfo> GetDbFieldByTableName(string tableName)
+        {
+            var db = DbContext.Instance;
+            var columms = db.DbMaintenance.GetColumnInfosByTableName(tableName);
+            return columms;
+        }
+
+
+        /// <summary>
+        /// 获取SQL查询结果
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        public async Task<List<dynamic>>  GetSqlResult(string sql)
+        {
+            // 检查SQL语句是否符合条件
+            if (IsInvalidSql(sql))
+            {
+                throw Oops.Oh("SQL不符合预期：sql只能包含查询、sql不能在select使用通配符*、sql只能返回单列");
+            }
+
+            // 执行查询
+            var result = _dbContext.Ado.SqlQueryAsync<dynamic>(sql).Result;
+            return result;
+        }
+
+        /// <summary>
+        /// 检查SQL语句是否符合条件
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        private bool IsInvalidSql(string sql)
+        {
+            // 转换SQL为小写
+            var lowerSql = sql.ToLower().Trim();
+
+            // 正则表达式检查是否包含删除、更新、插入语句
+            var forbiddenPatterns = new[]
+            {
+            @"^\s*delete\s+from\s+",
+            @"^\s*update\s+",
+            @"^\s*insert\s+into\s+"
+        };
+
+            // 正则表达式检查是否是select * 语句
+            var selectStarPatterns = new[]
+            {
+            @"^\s*select\s+\*\s+from\s+\w+\s*;",
+    @"^\s*select\s+\*\s+from\s+\w+\s*$",
+    @"^\s*select\s+\*\s+from\s+\w+\s+where\s+.*"
+        };
+
+            // 检查是否是删除、更新、插入语句
+            foreach (var pattern in forbiddenPatterns)
+            {
+                if (Regex.IsMatch(lowerSql, pattern))
+                {
+                    return true;
+                }
+            }
+
+            // 检查是否是select * 语句
+            foreach (var pattern in selectStarPatterns)
+            {
+                if (Regex.IsMatch(lowerSql, pattern))
+                {
+                    return true;
+                }
+            }
+
+            // 检查是否是SELECT语句并且只返回一列
+            if (IsMultipleColumnsSelect(sql))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// 检查SELECT语句是否返回多列
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        private bool IsMultipleColumnsSelect(string sql)
+        {
+            // 去除换行符
+            var sanitizedSql = sql.Replace("\n", " ").Replace("\r", " ");
+
+            // 使用正则表达式匹配SELECT语句的列部分
+            var selectColumnsPattern = @"^\s*select\s+(.*?)\s+from\s+";
+            var match = Regex.Match(sanitizedSql, selectColumnsPattern, RegexOptions.IgnoreCase);
+            if (match.Success)
+            {
+                // 提取列部分
+                var columnsPart = match.Groups[1].Value;
+
+                // 检查列部分是否包含多个列（使用逗号分隔）
+                var columns = columnsPart.Split(',');
+                if (columns.Length > 1)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
+
